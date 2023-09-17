e6009c1a26c7f0811c99fe773bd877d9bb063390
martin-242 <martin242@gmail.com>
2023-09-17 21:15:49 +0200

    Branding change

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> OmegaMindAI</span>
+        <span> OmegaMindAI </span>
       </a>
     </span>
   )
